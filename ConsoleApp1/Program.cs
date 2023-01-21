848b351ca8f1686607b5064b730a41831436644a
AndreyLinnik <av.linnik79@ya.ru>
2023-01-21 18:10:45 +0300

    add LaptopShop

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,60 @@
-﻿using System;
+﻿
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace individualLesson1
 {
-    internal class Program
+    internal class Laptop
     {
-        static void Main(string[] args)
+        private String model;
+        private int ram;
+        private int hdd;
+        private String os;
+        private int displaySise;
+
+        public Laptop(string model, int ram, int hdd, string os, int displaySise)
         {
+            this.model = model;
+            this.ram = ram;
+            this.hdd = hdd;
+            this.os = os;
+            this.displaySise = displaySise;
 
         }
+
+        public String getModel() { return model; }
+
+        public int getRam() { return ram; }
+
+        public int getHdd() { return hdd; }
+
+        public String getOs() { return os; }
+
+        public int getDisplaySise() { return displaySise; }
+
+
+
+
+
+
+
+
+
+
+
+        public override string? ToString()
+        {
+            return "Laptop: " + model;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
+
 }
