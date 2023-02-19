6380f10c6c1b1f8b6ebe13610849f1abb0886c70
Andrey Linnik <av.linnik79@ya.ru>
2023-02-19 12:32:20 +0300

    prepare task5

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SkillFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+
+        }
+
+        static (string name,string lastName, int age) Person()
+        {
+            (string name, string lastName, int age) person;
+            Console.WriteLine("введите имя ");
+            person.name=Console.ReadLine();
+            Console.WriteLine("введите фамилию");
+            person.lastName=Console.ReadLine();
+            //доббавить возраст
+// метод проверки ввода данных и list с кличками питомцев +
+// лист с любимыми цветами
+        }
+
+
+
+    }
+
+}
+
+
